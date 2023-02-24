d9829db311e7c6b1324c3e99811f3257099e458a
Leonardo Cserny <leo.cserny@gmail.com>
2023-02-24 20:10:45 +0200

    WIP prepare for cloud db sync

--- src/main/java/net/cserny/command/ServerCommandRepository.java
+++ src/main/java/net/cserny/command/ServerCommandRepository.java
@@ -6,4 +6,16 @@ import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class ServerCommandRepository implements PanacheMongoRepository<ServerCommand> {
+
+    public ServerCommand getByServerName(String serverName) {
+        return find("serverName", serverName).firstResult();
+    }
+
+    public void setLastPingDate(String serverName, long timestamp) {
+        update("lastPingDate", timestamp).where("serverName", serverName);
+    }
+
+    public void updateServerCommand(ServerCommand serverCommand) {
+        update(serverCommand);
+    }
 }
--- src/main/java/net/cserny/command/ServerCommandService.java
+++ src/main/java/net/cserny/command/ServerCommandService.java
@@ -1,12 +1,15 @@
 package net.cserny.command;
 
 import io.quarkus.arc.All;
+import io.quarkus.mongodb.panache.PanacheQuery;
 import io.quarkus.scheduler.Scheduled;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Dependent
 public class ServerCommandService {
@@ -22,18 +25,26 @@ public class ServerCommandService {
     List<Command> commands;
 
     @PostConstruct
-    public void cleanPendingActions() {
-        // TODO: when app starts empty pendingActions and add actionsAvailable (server can be controlled only when its online)
+    public void initServerCommand() {
+        ServerCommand server = repository.getByServerName(config.name());
+        server.actionsPending = new ArrayList<>();
+        server.actionsAvailable = commands.stream().map(Command::name).toList();
+        repository.updateServerCommand(server);
     }
 
     @Scheduled(every = "{server.command.listen.interval}")
     public void startListeningForActions() {
-        // TODO:
-        // - get serverCommand by serverName configured, save in var
-        // - update serverCommand lastPingDate to current timestamp unixnano
-        // - on serverCommand var
-        //   - get first actionPending, save to var
-        //   - update serverCommand actionsPending to remaining actionsPending (without the one saved to var)
-        //   - execute the action from the var
+        repository.setLastPingDate(config.name(), System.currentTimeMillis());
+
+        ServerCommand server = repository.getByServerName(config.name());
+        if (!server.actionsPending.isEmpty()) {
+            String firstActionPending = server.actionsPending.remove(0);
+            repository.updateServerCommand(server);
+            for (Command command : commands) {
+                if (command.name().equals(firstActionPending)) {
+                    command.execute();
+                }
+            }
+        }
     }
 }
