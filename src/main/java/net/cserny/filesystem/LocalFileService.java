de2e00163e2a37b804f8a67c0ebe44cc7d141298
Leonardo Cserny <leo.cserny@gmail.com>
2023-02-27 20:31:06 +0200

    fixes local file service walk

--- src/main/java/net/cserny/filesystem/LocalFileService.java
+++ src/main/java/net/cserny/filesystem/LocalFileService.java
@@ -42,12 +42,21 @@ public class LocalFileService {
         Files.move(source.path(), dest.path());
     }
 
-    public List<Path> walk(LocalPath path, int maxDepth) throws IOException {
+    public List<Path> walk(LocalPath path, int maxDepthFromPath) throws IOException {
+        if (!Files.isDirectory(path.path())) {
+            throw new NotDirectoryException(path.path().toString());
+        }
+
+        if (maxDepthFromPath < 1) {
+            throw new IllegalArgumentException("Max depth passed cannot be lower than 1");
+        }
+
         List<Path> files;
-        try (Stream<Path> walkStream = Files.walk(path.path(), maxDepth, FileVisitOption.FOLLOW_LINKS)) {
+        try (Stream<Path> walkStream = Files.walk(path.path(), maxDepthFromPath, FileVisitOption.FOLLOW_LINKS)) {
             files = walkStream.filter(Files::isRegularFile)
                     .collect(Collectors.toList());
         }
+
         return files;
     }
 }
--- src/test/java/net/cserny/filesystem/LocalFileServiceTest.java
+++ src/test/java/net/cserny/filesystem/LocalFileServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -73,6 +74,58 @@ public class LocalFileServiceTest {
         assertEquals(path1, filesFound.get(0).toAbsolutePath().toString());
         assertEquals(path2, filesFound.get(1).toAbsolutePath().toString());
         assertEquals(path4, filesFound.get(2).toAbsolutePath().toString());
+    }
+
+    @Test
+    @DisplayName("Service can walk a path of max depth passed from path passed")
+    public void walkDepthIsCorrect() throws IOException {
+        LocalPath rootPath = service.produceLocalPath("/mypath/main/movies");
+        String path1 = "/mypath/main/movies/movie1.mp4";
+        String path2 = "/mypath/main/movies/movie2/file1.mp4";
+        String path3 = "/mypath/main/movies/mypath/movie3/file3.mkv";
+        String path4 = "/mypath/main/movies/movie2.mp4";
+
+        createFile(path1);
+        createFile(path2);
+        createFile(path3);
+        createFile(path4);
+
+        List<Path> firstFiles = service.walk(rootPath, 1);
+        assertEquals(2, firstFiles.size());
+        assertEquals(path1, firstFiles.get(0).toAbsolutePath().toString());
+        assertEquals(path4, firstFiles.get(1).toAbsolutePath().toString());
+
+        List<Path> secondFiles = service.walk(rootPath, 2);
+        assertEquals(3, secondFiles.size());
+        assertEquals(path1, secondFiles.get(0).toAbsolutePath().toString());
+        assertEquals(path2, secondFiles.get(1).toAbsolutePath().toString());
+        assertEquals(path4, secondFiles.get(2).toAbsolutePath().toString());
+
+        List<Path> thirdFiles = service.walk(rootPath, 3);
+        assertEquals(4, thirdFiles.size());
+        assertEquals(path1, thirdFiles.get(0).toAbsolutePath().toString());
+        assertEquals(path2, thirdFiles.get(1).toAbsolutePath().toString());
+        assertEquals(path4, thirdFiles.get(2).toAbsolutePath().toString());
+        assertEquals(path3, thirdFiles.get(3).toAbsolutePath().toString());
+    }
+
+    @Test
+    @DisplayName("Service can walk a path at least depth 1")
+    public void walkDepthCannotBeZero() throws IOException {
+        LocalPath rootPath = service.produceLocalPath("/mypath");
+        createFile("/mypath/doesntMatter.txt");
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.walk(rootPath, 0);
+        });
+    }
+
+    @Test
+    @DisplayName("Service can walk a directory only")
+    public void walkPathMustBeDirectory() throws IOException {
+        LocalPath rootPath = service.produceLocalPath("/mypath");
+        assertThrows(NotDirectoryException.class, () -> {
+            service.walk(rootPath, 1);
+        });
     }
 
     private LocalPath createFile(String somePath) throws IOException {
