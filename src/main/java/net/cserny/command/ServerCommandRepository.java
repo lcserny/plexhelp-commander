4e1646462c0f685d6a45f8297fc761c0b167a126
Leonardo Cserny <leo.cserny@gmail.com>
2023-08-20 22:33:50 +0300

    minor fix

--- src/main/java/net/cserny/command/ServerCommandRepository.java
+++ src/main/java/net/cserny/command/ServerCommandRepository.java
@@ -3,7 +3,6 @@ package net.cserny.command;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
