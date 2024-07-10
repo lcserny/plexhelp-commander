f4db35b02cd194bf51d253df3b582ec962a8b8fb
Leonardo Cserny <leo.cserny@gmail.com>
2024-07-10 22:10:35 +0300

    test commands

--- src/main/java/net/cserny/command/LinuxEnvConditional.java
+++ src/main/java/net/cserny/command/LinuxEnvConditional.java
@@ -1,21 +1,22 @@
 package net.cserny.command;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import java.util.Objects;
 
+@Slf4j
 public class LinuxEnvConditional implements Condition {
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        boolean isLinux = (Objects.requireNonNull(context.getEnvironment().getProperty("os.name")).contains("nux")
-                || Objects.requireNonNull(context.getEnvironment().getProperty("os.name")).contains("aix"));
-
-        if (isLinux) {
-            String wsl = context.getEnvironment().getProperty("server.command.wsl");
-            return !Boolean.parseBoolean(wsl);
+        String wsl = context.getEnvironment().getProperty("server.command.wsl");
+        if (Boolean.parseBoolean(wsl)) {
+            log.info("wsl is true, returning false");
+            return false;
         }
 
-        return false;
+        return (Objects.requireNonNull(context.getEnvironment().getProperty("os.name")).contains("nux")
+                || Objects.requireNonNull(context.getEnvironment().getProperty("os.name")).contains("aix"));
     }
 }
--- src/main/java/net/cserny/command/WindowsEnvConditional.java
+++ src/main/java/net/cserny/command/WindowsEnvConditional.java
@@ -1,17 +1,21 @@
 package net.cserny.command;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import java.util.Objects;
 
+@Slf4j
 public class WindowsEnvConditional implements Condition {
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         String wsl = context.getEnvironment().getProperty("server.command.wsl");
-        boolean isWsl = Boolean.parseBoolean(wsl);
-        boolean isWindows = Objects.requireNonNull(context.getEnvironment().getProperty("os.name")).contains("Win");
+        if (Boolean.parseBoolean(wsl)) {
+            log.info("wsl is true, returning true");
+            return true;
+        }
 
-        return isWindows || isWsl;
+        return Objects.requireNonNull(context.getEnvironment().getProperty("os.name")).contains("Win");
     }
 }
