21246a18c31966f7d53edf6c0f8a5f42eff94bc6
Leonardo Cserny <leo.cserny@gmail.com>
2024-08-06 16:55:10 +0300

    refactor shutdown

--- src/main/java/net/cserny/command/restart/RestartCommand.java
+++ src/main/java/net/cserny/command/restart/RestartCommand.java
@@ -1,61 +1,20 @@
 package net.cserny.command.restart;
 
 import lombok.extern.slf4j.Slf4j;
-import net.cserny.command.Command;
-import net.cserny.command.CommandResponse;
-import net.cserny.command.ServerCommandProperties;
-import net.cserny.command.shutdown.ShutdownCommand;
+import net.cserny.command.AbstractShutdownCommand;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 @Component
-public class RestartCommand implements Command {
+public class RestartCommand extends AbstractShutdownCommand {
 
     public static final String NAME = "reboot";
 
-    @Autowired
-    ServerCommandProperties properties;
-
-    @Override
-    public CommandResponse execute(String[] inParams) {
-        validate();
-
-        List<String> outParams = new ArrayList<>();
-        outParams.add(getCommand());
-        appendParams(outParams, inParams);
-
-        try {
-            Runtime runtime = Runtime.getRuntime();
-            runtime.exec(outParams.toArray(new String[0]));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return CommandResponse.EMPTY;
-    }
-
-    private void validate() {
-        if (!SystemUtils.IS_OS_WINDOWS && !SystemUtils.IS_OS_LINUX) {
-            throw new RuntimeException("Unsupported operating system: " + SystemUtils.OS_NAME);
-        }
-    }
-
-    private void appendParams(List<String> outParams, String[] inParams) {
-        if (SystemUtils.IS_OS_WINDOWS || properties.isWsl()) {
-            appendWindowsParams(outParams, inParams);
-        } else if (SystemUtils.IS_OS_LINUX) {
-            appendLinuxParams(outParams, inParams);
-        }
-    }
-
-    private void appendLinuxParams(List<String> outParams, String[] inParams) {
+    protected void appendLinuxParams(List<String> outParams, String[] inParams) {
         if (inParams.length == 0) {
             outParams.add("-r");
             outParams.add("now");
@@ -67,7 +26,7 @@ public class RestartCommand implements Command {
         }
     }
 
-    private void appendWindowsParams(List<String> outParams, String[] inParams) {
+    protected void appendWindowsParams(List<String> outParams, String[] inParams) {
         if (inParams.length == 0) {
             outParams.add("-r");
         } else if (inParams.length == 1 && StringUtils.isNumeric(inParams[0])) {
@@ -77,15 +36,6 @@ public class RestartCommand implements Command {
         } else {
             Collections.addAll(outParams, inParams);
         }
-    }
-
-    private String getCommand() {
-        if (properties.isWsl() && properties.getWslOverrides().containsKey(ShutdownCommand.NAME)) {
-            String override = properties.getWslOverrides().get(ShutdownCommand.NAME);
-            log.info("Using WSL override {}", override);
-            return override;
-        }
-        return ShutdownCommand.NAME;
     }
 
     @Override
--- src/main/java/net/cserny/command/shutdown/ShutdownCommand.java
+++ src/main/java/net/cserny/command/shutdown/ShutdownCommand.java
@@ -1,60 +1,20 @@
 package net.cserny.command.shutdown;
 
 import lombok.extern.slf4j.Slf4j;
-import net.cserny.command.Command;
-import net.cserny.command.CommandResponse;
-import net.cserny.command.ServerCommandProperties;
+import net.cserny.command.AbstractShutdownCommand;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 @Component
-public class ShutdownCommand implements Command {
+public class ShutdownCommand extends AbstractShutdownCommand {
 
     public static final String NAME = "shutdown";
 
-    @Autowired
-    ServerCommandProperties properties;
-
-    @Override
-    public CommandResponse execute(String[] inParams) {
-        validate();
-
-        List<String> outParams = new ArrayList<>();
-        outParams.add(getCommand());
-        appendParams(outParams, inParams);
-
-        try {
-            Runtime runtime = Runtime.getRuntime();
-            runtime.exec(outParams.toArray(new String[0]));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return CommandResponse.EMPTY;
-    }
-
-    private void validate() {
-        if (!SystemUtils.IS_OS_WINDOWS && !SystemUtils.IS_OS_LINUX) {
-            throw new RuntimeException("Unsupported operating system: " + SystemUtils.OS_NAME);
-        }
-    }
-
-    private void appendParams(List<String> outParams, String[] inParams) {
-        if (SystemUtils.IS_OS_WINDOWS || properties.isWsl()) {
-            appendWindowsParams(outParams, inParams);
-        } else if (SystemUtils.IS_OS_LINUX) {
-            appendLinuxParams(outParams, inParams);
-        }
-    }
-
-    private void appendLinuxParams(List<String> outParams, String[] inParams) {
+    protected void appendLinuxParams(List<String> outParams, String[] inParams) {
         if (inParams.length == 0) {
             outParams.add("now");
         } else if (inParams.length == 1 && StringUtils.isNumeric(inParams[0])) {
@@ -64,7 +24,7 @@ public class ShutdownCommand implements Command {
         }
     }
 
-    private void appendWindowsParams(List<String> outParams, String[] inParams) {
+    protected void appendWindowsParams(List<String> outParams, String[] inParams) {
         if (inParams.length == 0) {
             outParams.add("-s");
         } else if (inParams.length == 1 && StringUtils.isNumeric(inParams[0])) {
@@ -74,15 +34,6 @@ public class ShutdownCommand implements Command {
         } else {
             Collections.addAll(outParams, inParams);
         }
-    }
-
-    private String getCommand() {
-        if (properties.isWsl() && properties.getWslOverrides().containsKey(NAME)) {
-            String override = properties.getWslOverrides().get(NAME);
-            log.info("Using WSL override {}", override);
-            return override;
-        }
-        return NAME;
     }
 
     @Override
