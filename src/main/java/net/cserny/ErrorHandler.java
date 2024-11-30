28d36b9ce7dda1e740c2ba95e21ab2f8fe32b651
Leonardo Cserny <leo.cserny@gmail.com>
2024-11-30 12:38:00 +0200

    impl error handler

---
+++ src/main/java/net/cserny/ErrorHandler.java
@@ -0,0 +1,23 @@
+package net.cserny;
+
+import lombok.extern.slf4j.Slf4j;
+import net.cserny.generated.ApplicationErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApplicationErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+
+        return new ResponseEntity<>(ApplicationErrorResponse.builder()
+                .error(e.getClass().getName())
+                .message(e.getMessage())
+                .build(), HttpStatus.BAD_REQUEST);
+    }
+}
