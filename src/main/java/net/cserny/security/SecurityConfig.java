95c3180a9b1d79b6a39c55aeec91e9563a2504a9
Leonardo Cserny <leo.cserny@gmail.com>
2025-01-11 15:32:20 +0200

    fix security torrents

--- src/main/java/net/cserny/security/SecurityConfig.java
+++ src/main/java/net/cserny/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((registry) -> registry
+                        .requestMatchers(HttpMethod.POST, "**/torrents").permitAll()
                         .requestMatchers(HttpMethod.GET)
                             .hasAnyAuthority(UserPerm.READ.toString(), UserRole.ADMIN.toString())
                         .requestMatchers(HttpMethod.POST)
