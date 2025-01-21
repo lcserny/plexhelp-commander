525b2922d707b7975b703e6c984d5e44546871ad
Leonardo Cserny <leo.cserny@gmail.com>
2025-01-21 16:35:57 +0200

    desperation

--- src/main/java/net/cserny/filesystem/FileServiceCacheLogger.java
+++ src/main/java/net/cserny/filesystem/FileServiceCacheLogger.java
@@ -1,25 +1,25 @@
-package net.cserny.filesystem;
-
-import lombok.extern.slf4j.Slf4j;
-import net.cserny.LRUCache;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.nio.file.attribute.BasicFileAttributes;
-
-@Slf4j
-@Component
-@ConditionalOnProperty(prefix = "filesystem.cache", name = {"enabled", "logger-enabled"}, havingValue = "true")
-public class FileServiceCacheLogger {
-
-    @Autowired
-    private CachedLocalFileService fileService;
-
-    @Scheduled(initialDelayString = "${filesystem.cache.initial-delay-ms}", fixedDelayString = "${filesystem.cache.cron-ms}")
-    public void runCacheLogging() {
-        LRUCache<String, BasicFileAttributes> fileAttrCache = fileService.getFileAttrCache();
-        log.info("Current FileService cache size {} and cacheMisses {}", fileAttrCache.size(), fileAttrCache.cacheMisses());
-    }
-}
+//package net.cserny.filesystem;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import net.cserny.LRUCache;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.nio.file.attribute.BasicFileAttributes;
+//
+//@Slf4j
+//@Component
+//@ConditionalOnProperty(prefix = "filesystem.cache", name = {"enabled", "logger-enabled"}, havingValue = "true")
+//public class FileServiceCacheLogger {
+//
+//    @Autowired
+//    private CachedLocalFileService fileService;
+//
+//    @Scheduled(initialDelayString = "${filesystem.cache.initial-delay-ms}", fixedDelayString = "${filesystem.cache.cron-ms}")
+//    public void runCacheLogging() {
+//        LRUCache<String, BasicFileAttributes> fileAttrCache = fileService.getFileAttrCache();
+//        log.info("Current FileService cache size {} and cacheMisses {}", fileAttrCache.size(), fileAttrCache.cacheMisses());
+//    }
+//}
