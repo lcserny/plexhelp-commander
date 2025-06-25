f098ce0320cb36249d0d945da16d17f52777e81f
Leonardo Cserny <leo.cserny@gmail.com>
2025-06-25 22:51:18 +0300

    fix shutdown

--- src/main/java/net/cserny/command/restart/RestartCommand.java
+++ src/main/java/net/cserny/command/restart/RestartCommand.java
@@ -41,19 +41,18 @@ public class RestartCommand extends AbstractOSCommand {
 
     @Override
     protected Process executeInternalWindows(Runtime runtime, String[] params) throws IOException {
-        String commandBase = getCommandPrefix() + "powershell.exe";
+        String commandBase = getCommandPrefix() + "shutdown.exe";
 
         if (params.length == 0) {
             log.info("Rebooting system without delay");
-            return runtime.exec(new String[]{commandBase, "shutdown", "-r", "-f"});
+            return runtime.exec(new String[]{commandBase, "-r", "-f"});
         } else if (params.length == 1 && StringUtils.isNumeric(params[0])) {
             log.info("Rebooting system in {} minutes", params[0]);
-            return runtime.exec(new String[]{commandBase, "shutdown", "-r", "-f", "-t", String.valueOf(60 * Integer.parseInt(params[0]))});
+            return runtime.exec(new String[]{commandBase, "-r", "-f", "-t", String.valueOf(60 * Integer.parseInt(params[0]))});
         } else {
             log.info("Rebooting system with params: {}", Arrays.toString(params));
             List<String> execParams = new ArrayList<>();
             execParams.add(commandBase);
-            execParams.add("shutdown");
             execParams.addAll(List.of(params));
 
             return runtime.exec(execParams.toArray(new String[0]));
--- src/main/java/net/cserny/command/shutdown/ShutdownCommand.java
+++ src/main/java/net/cserny/command/shutdown/ShutdownCommand.java
@@ -41,19 +41,18 @@ public class ShutdownCommand extends AbstractOSCommand {
 
     @Override
     protected Process executeInternalWindows(Runtime runtime, String[] params) throws IOException {
-        String commandBase = getCommandPrefix() + "powershell.exe";
+        String commandBase = getCommandPrefix() + "shutdown.exe";
 
         if (params.length == 0) {
             log.info("Shutting down system without delay");
-            return runtime.exec(new String[]{commandBase, "shutdown", "-s", "-f"});
+            return runtime.exec(new String[]{commandBase, "-s", "-f"});
         } else if (params.length == 1 && StringUtils.isNumeric(params[0])) {
             log.info("Shutting down system in {} minutes", params[0]);
-            return runtime.exec(new String[]{commandBase, "shutdown", "-s", "-f", "-t", String.valueOf(60 * Integer.parseInt(params[0]))});
+            return runtime.exec(new String[]{commandBase, "-s", "-f", "-t", String.valueOf(60 * Integer.parseInt(params[0]))});
         } else {
             log.info("Shutting down system with params: {}", Arrays.toString(params));
             List<String> execParams = new ArrayList<>();
             execParams.add(commandBase);
-            execParams.add("shutdown");
             execParams.addAll(List.of(params));
 
             return runtime.exec(execParams.toArray(new String[0]));
