eb14bbc2fd693c02e044344bf75d882c0f6ec730
Leonardo Cserny <leo.cserny@gmail.com>
2026-02-04 22:58:18 +0200

    fix event publisher

--- src/main/java/net/cserny/support/events/CommanderEventPublisher.java
+++ src/main/java/net/cserny/support/events/CommanderEventPublisher.java
@@ -11,12 +11,17 @@ public class CommanderEventPublisher {
 
     private final ApplicationEventPublisher publisher;
 
-    public <E extends SyncEvent<?>> void processSync(E event) {
+    public <R, E extends SyncEvent<R>> R process(E event) {
         publisher.publishEvent(event);
+        R result = event.getResult();
+        if (result == null) {
+            throw new RuntimeException("Event was not processed correctly, no result was returned by an EventListener");
+        }
+        return result;
     }
 
     @Async
-    public <E extends AsyncEvent> void sendAsync(E event) {
+    public <E extends AsyncEvent> void send(E event) {
         publisher.publishEvent(event);
     }
 }
