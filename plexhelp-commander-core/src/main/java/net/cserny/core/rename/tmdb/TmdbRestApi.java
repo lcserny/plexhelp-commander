74c24caae98fa772845b21cec2d507d7aaa9754c
Leonardo Cserny <leo.cserny@gmail.com>
2026-07-05 21:55:54 +0300

    fix

--- plexhelp-commander-core/src/main/java/net/cserny/core/rename/tmdb/TmdbRestApi.java
+++ plexhelp-commander-core/src/main/java/net/cserny/core/rename/tmdb/TmdbRestApi.java
@@ -3,32 +3,35 @@ package net.cserny.core.rename.tmdb;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
-import org.springframework.web.service.annotation.PostExchange;
 
 import java.util.List;
 
-@HttpExchange(TmdbRestApi.Routes.BASE_URI)
+@HttpExchange(value = TmdbRestApi.Routes.BASE_URI, accept = MediaType.APPLICATION_JSON_VALUE, contentType = MediaType.APPLICATION_JSON_VALUE)
 public interface TmdbRestApi {
 
-    @PostExchange(TmdbRestApi.Routes.SEARCH_MOVIES_URI)
-    MovieResults searchMovies(@PathVariable String query, @PathVariable Integer year);
+    @GetExchange(Routes.SEARCH_MOVIES_URI)
+    MovieResults searchMovies(@PathVariable String query, @RequestParam(required = false) Integer year);
 
-    @PostExchange(Routes.SEARCH_TV_URI)
-    TvResults searchTvShows(@PathVariable String query, @PathVariable Integer year);
+    @GetExchange(Routes.SEARCH_TV_URI)
+    TvResults searchTvShows(@PathVariable String query, @RequestParam(required = false) Integer year);
 
-    @PostExchange(Routes.MOVIE_CREDITS_URI)
+    @GetExchange(Routes.MOVIE_CREDITS_URI)
     Credits movieCredits(@PathVariable Integer movieId);
 
-    @PostExchange(Routes.TV_CREDITS_URI)
+    @GetExchange(Routes.TV_CREDITS_URI)
     Credits tvShowCredits(@PathVariable Integer tvId);
 
     interface Routes {
 
         String BASE_URI = "/3";
-        String SEARCH_MOVIES_URI = "/search/movie?api_key={tmdbApiKey}&query={query}&year={year}";
-        String SEARCH_TV_URI = "/search/tv?api_key={tmdbApiKey}&query={query}&year={year}";
+        // Note: year query param is optional so its not hardcoded in URI template
+        String SEARCH_MOVIES_URI = "/search/movie?api_key={tmdbApiKey}&query={query}";
+        String SEARCH_TV_URI = "/search/tv?api_key={tmdbApiKey}&query={query}";
         String MOVIE_CREDITS_URI = "/movie/{movieId}/credits?api_key={tmdbApiKey}";
         String TV_CREDITS_URI = "/tv/{tvId}/credits?api_key={tmdbApiKey}";
     }
